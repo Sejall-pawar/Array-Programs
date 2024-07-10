3ceb4f262f7244497833c95a155c20f3f1320979
Sejall-pawar <sejalpawar463@gmail.com>
2024-07-10 20:45:46 +0530

    added function to sum the odd numbers from array

--- sum.js
+++ sum.js
@@ -1,1 +1,13 @@
 const numbers = [23,67,88,44,56,76,43]
+
+let sum = 0;
+
+for(let i=0; i<numbers.length; i++)
+{
+    if(numbers[i]%2==1)
+    {
+        sum+= numbers[i]
+    }
+}
+
+console.log(`Sum of Odd numbers are ${sum}`)
