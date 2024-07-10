ef1e6a8118ebee5d5f50a694c1207265201d7c84
Sejall-pawar <sejalpawar463@gmail.com>
2024-07-10 20:09:20 +0530

    added function to find largest number

--- large.js
+++ large.js
@@ -1,1 +1,12 @@
 const age = [20,17,32,12,27]
+
+let largest = age[0]
+
+for (let i=1; i<age.length; i++)
+{
+    if(age[i] > largest){
+        largest = age[i]
+    }
+}
+
+console.log(`The maximumg age is ${largest}`)
